aa133f4bb1910db7b94b432a2bef5fbd646bfdb4
ryanlea <ryan.lea@gmail.com>
2016-12-04 22:28:08 +1100

    Moved quickfixj under samples. Added fix4j-client to samples.

---
+++ samples/fix4j-samples/fix4j-client/src/main/java/org/fix4j/client/Fix4jClient.java
@@ -0,0 +1,91 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016 fix4j.org (tools4j.org)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.fix4j.client;
+
+import org.fix4j.engine.Application;
+import org.fix4j.engine.ExceptionHandler;
+import org.fix4j.engine.FixEngine;
+import org.fix4j.engine.StringMessage;
+import org.fix4j.engine.log.LogEntry;
+import org.fix4j.engine.log.MessageLog;
+import org.fix4j.engine.log.UnsafeMessageLog;
+import org.fix4j.engine.net.TcpConnectionHandler;
+import org.fix4j.engine.net.TcpConnectionInitiator;
+import org.fix4j.engine.net.TcpExceptionHandler;
+import org.fix4j.engine.session.FixSessionInitiator;
+import org.fix4j.engine.spec.Fix4SessionLifecycle;
+
+import java.io.IOException;
+import java.util.function.Consumer;
+
+/**
+ * Created by ryan on 4/12/16.
+ */
+public class Fix4jClient {
+
+    public static void main(final String[] args) throws IOException {
+        new Fix4jClient().run();
+    }
+
+    private void run() throws IOException {
+        final TcpConnectionHandler tcpConnectionHandler = new TcpConnectionHandler(TcpExceptionHandler.throwing());
+        final FixEngine fixEngine = new FixEngine(tcpConnectionHandler, ExceptionHandler.logging());
+        fixEngine.start();
+
+        final int TWO_MB = 2 * 1024 * 1024;
+        final TcpConnectionInitiator tcpConnectionInitiator = new TcpConnectionInitiator("localhost", 12000, TcpExceptionHandler.throwing(), tcpConnectionHandler);
+        final MessageLog initiatorInbound = new UnsafeMessageLog("./logs/inbound.log", TWO_MB, ExceptionHandler.throwing());
+        final MessageLog initiatorOutbound = new UnsafeMessageLog("./logs/outbound.log", TWO_MB, ExceptionHandler.throwing());
+        final FixSessionInitiator initiator = new FixSessionInitiator(
+                tcpConnectionInitiator,
+                initiatorInbound,
+                initiatorOutbound,
+                () -> new Fix4SessionLifecycle.Initiator(sessionMessageType -> {
+                    switch (sessionMessageType) {
+                        case LOGON:
+                            return new StringMessage("logon");
+                    }
+                    return null;
+                }), Fix4jClientApplication::new
+                );
+        fixEngine.register(initiator);
+
+        while (!Thread.interrupted()) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
+
+    private static final class Fix4jClientApplication implements Application {
+
+        @Override
+        public Consumer<LogEntry> consume() {
+            return null;
+        }
+    }
+
+}
